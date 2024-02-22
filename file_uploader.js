a258c939f7ce9c380640dfb27f34798565d47b9c
Vasu Chawla <vchawla26@gmail.com>
2024-02-22 18:49:18 +0530

    Create file_uploader.js

---
+++ file_uploader.js
@@ -0,0 +1,63 @@
+react-native-document-picker
+
+
+import DocumentPicker, {pick, types} from 'react-native-document-picker';
+
+<Button onPress={addNew}>Choose and upload file</Button>
+
+const addNew = async () => {
+    try {
+      const pickerResult = await DocumentPicker.pickSingle({
+        presentationStyle: 'fullScreen',
+        // copyTo: 'cachesDirectory',
+        type: [types.pdf, types.images],
+      });
+      uploadFile(pickerResult);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const uploadFile = async function (data) {
+    // setLoading(true);
+    const resp = await imageUploader(data, token);
+    if (resp.ok) {
+      console.log( resp);
+    } else {
+      Alert.alert(t('common.error'), t('common.failed-to-upload-file'));
+    //   setLoading(false);
+    }
+  };
+
+
+
+const imageUploader = async (file, token) => {
+    var data = new FormData();
+  
+    try {
+      data.append('files', {
+        uri:
+          Platform.OS === 'ios'
+            ? file.uri.replace('file://', '')
+            : file.uri,
+        name: file.fileName || file.name,
+        type: file.type,
+      });
+  
+      const response = await axios.post(basePath+'/api/upload', data, {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      if (response?.data  ) {
+        return {ok: true, id: response.id, url: response.url};
+      } else {
+        return {ok: false};
+      }
+    } catch (err) {
+      console.log(err);
+      return {ok: false};
+    }
+  };
+  
